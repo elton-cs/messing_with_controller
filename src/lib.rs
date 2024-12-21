fa15df44b6b591c52a2f4070ebf51a02b21e79a6
zkl10 <govada.elton@gmail.com>
2024-12-21 18:18:40 -0500

    Start new game transaction

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub mod crosswordle_crosswordle;
+// pub mod crosswordle_crosswordle;
 // pub mod crosswordle_m_game;
 // pub mod crosswordle_m_progress;
 // pub mod crosswordle_m_wordle;
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use account_sdk::{
     provider,
     signers::{Owner, Signer},
 };
+// use messing_with_controller::crosswordle_crosswordle::CrosswordleCrosswordle;
 use starknet::{
     accounts::{Account, ExecutionEncoding, SingleOwnerAccount},
-    core::types::{Call, Felt},
+    core::{
+        codec::Encode,
+        types::{ByteArray, Call, Felt},
+    },
     providers::{jsonrpc::HttpTransport, JsonRpcClient, Provider, Url},
     signers::{LocalWallet, SigningKey},
 };
@@ -23,8 +27,8 @@ const WORLD_ADDRESS: &str = "0x02f69de741bf133c6ffc205439bebdb5f26f0271dcc1cbb65
 
 #[tokio::main]
 async fn main() {
-    let secret_scalar = Felt::from_hex_unchecked(PRIVATE_KEY);
-    let owner = Signer::Starknet(SigningKey::from_secret_scalar(secret_scalar));
+    // let secret_scalar = Felt::from_hex_unchecked(PRIVATE_KEY);
+    // let owner = Signer::Starknet(SigningKey::from_secret_scalar(secret_scalar));
 
     let provider = JsonRpcClient::new(HttpTransport::new(Url::parse(RPC_URL).unwrap()));
     let chain_id = provider.chain_id().await.unwrap();
@@ -36,21 +40,31 @@ async fn main() {
     let mut account =
         SingleOwnerAccount::new(provider, signer, address, chain_id, ExecutionEncoding::New);
 
-    let start_game_manual = account.execute_v1(vec![Call {
-        to: Felt::from_hex_unchecked(
-            "0x2ccc78c905d9fa42a542b8de8c438559ebac7eaecedf5cd932ffbd46fd98729",
-        ),
-        selector: starknet::core::utils::get_selector_from_name("start_game").unwrap(),
-        calldata: vec![],
-    }]);
+    // let start_game_manual = account.execute_v1(vec![Call {
+    //     to: Felt::from_hex_unchecked(
+    //         "0x2ccc78c905d9fa42a542b8de8c438559ebac7eaecedf5cd932ffbd46fd98729",
+    //     ),
+    //     selector: starknet::core::utils::get_selector_from_name("start_game").unwrap(),
+    //     calldata: vec![],
+    // }]);
 
+    let guess: ByteArray = "STARK".into();
+    let mut guess_felts = vec![];
+    guess.encode(&mut guess_felts).unwrap();
     let make_guess = account.execute_v1(vec![Call {
         to: Felt::from_hex_unchecked(
             "0x2ccc78c905d9fa42a542b8de8c438559ebac7eaecedf5cd932ffbd46fd98729",
         ),
-        selector: starknet::core::utils::get_selector_from_name("make_guess").unwrap(),
-        calldata: vec![],
+        selector: starknet::core::utils::get_selector_from_name("submit_guess").unwrap(),
+        calldata: guess_felts,
     }]);
+    let transaction2 = make_guess.send().await.unwrap();
 
-    let result = start_game_manual.send().await.unwrap();
+    // let crosswordle = CrosswordleCrosswordle::new(
+    //     Felt::from_hex_unchecked(
+    //         "0x2ccc78c905d9fa42a542b8de8c438559ebac7eaecedf5cd932ffbd46fd98729",
+    //     ),
+    //     account,
+    // );
+    // let transaction1 = crosswordle.start_game().call().await.unwrap();
 }
