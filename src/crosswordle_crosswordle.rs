fc7919bc096f1c3e1b7f682d1c982eadd4cd619c
zkl10 <govada.elton@gmail.com>
2024-12-21 20:23:28 -0500

    Create transaction from cainome code gen

--- src/crosswordle_crosswordle.rs
+++ src/crosswordle_crosswordle.rs
@@ -122,6 +122,64 @@ impl Upgraded {
         "Upgraded"
     }
 }
+#[derive()]
+pub enum Event {}
+impl cainome::cairo_serde::CairoSerde for Event {
+    type RustType = Self;
+    const SERIALIZED_SIZE: std::option::Option<usize> = std::option::Option::None;
+    #[inline]
+    fn cairo_serialized_size(__rust: &Self::RustType) -> usize {
+        match __rust {
+            _ => 0,
+        }
+    }
+    fn cairo_serialize(__rust: &Self::RustType) -> Vec<starknet::core::types::Felt> {
+        match __rust {
+            _ => vec![],
+        }
+    }
+    fn cairo_deserialize(
+        __felts: &[starknet::core::types::Felt],
+        __offset: usize,
+    ) -> cainome::cairo_serde::Result<Self::RustType> {
+        let __f = __felts[__offset];
+        let __index = u128::from_be_bytes(__f.to_bytes_be()[16..].try_into().unwrap());
+        match __index as usize {
+            _ => {
+                return Err(cainome::cairo_serde::Error::Deserialize(format!(
+                    "Index not handle for enum {}",
+                    "Event"
+                )))
+            }
+        }
+    }
+}
+impl TryFrom<&starknet::core::types::EmittedEvent> for Event {
+    type Error = String;
+    fn try_from(event: &starknet::core::types::EmittedEvent) -> Result<Self, Self::Error> {
+        use cainome::cairo_serde::CairoSerde;
+        if event.keys.is_empty() {
+            return Err("Event has no key".to_string());
+        }
+        Err(format!(
+            "Could not match any event from keys {:?}",
+            event.keys
+        ))
+    }
+}
+impl TryFrom<&starknet::core::types::Event> for Event {
+    type Error = String;
+    fn try_from(event: &starknet::core::types::Event) -> Result<Self, Self::Error> {
+        use cainome::cairo_serde::CairoSerde;
+        if event.keys.is_empty() {
+            return Err("Event has no key".to_string());
+        }
+        Err(format!(
+            "Could not match any event from keys {:?}",
+            event.keys
+        ))
+    }
+}
 
 impl<A: starknet::accounts::ConnectedAccount + Sync> CrosswordleCrosswordle<A> {
     #[allow(clippy::ptr_arg)]
@@ -152,36 +210,6 @@ impl<A: starknet::accounts::ConnectedAccount + Sync> CrosswordleCrosswordle<A> {
     }
     #[allow(clippy::ptr_arg)]
     #[allow(clippy::too_many_arguments)]
-    pub fn start_game(&self) -> cainome::cairo_serde::call::FCall<A::Provider, ()> {
-        use cainome::cairo_serde::CairoSerde;
-        let mut __calldata = vec![];
-        let __call = starknet::core::types::FunctionCall {
-            contract_address: self.address,
-            entry_point_selector: starknet::macros::selector!("start_game"),
-            calldata: __calldata,
-        };
-        cainome::cairo_serde::call::FCall::new(__call, self.provider())
-    }
-    #[allow(clippy::ptr_arg)]
-    #[allow(clippy::too_many_arguments)]
-    pub fn submit_guess(
-        &self,
-        word_byte_array: &cainome::cairo_serde::ByteArray,
-    ) -> cainome::cairo_serde::call::FCall<A::Provider, ()> {
-        use cainome::cairo_serde::CairoSerde;
-        let mut __calldata = vec![];
-        __calldata.extend(cainome::cairo_serde::ByteArray::cairo_serialize(
-            word_byte_array,
-        ));
-        let __call = starknet::core::types::FunctionCall {
-            contract_address: self.address,
-            entry_point_selector: starknet::macros::selector!("submit_guess"),
-            calldata: __calldata,
-        };
-        cainome::cairo_serde::call::FCall::new(__call, self.provider())
-    }
-    #[allow(clippy::ptr_arg)]
-    #[allow(clippy::too_many_arguments)]
     pub fn world_dispatcher(
         &self,
     ) -> cainome::cairo_serde::call::FCall<A::Provider, IWorldDispatcher> {
@@ -193,6 +221,64 @@ impl<A: starknet::accounts::ConnectedAccount + Sync> CrosswordleCrosswordle<A> {
             calldata: __calldata,
         };
         cainome::cairo_serde::call::FCall::new(__call, self.provider())
+    }
+    #[allow(clippy::ptr_arg)]
+    #[allow(clippy::too_many_arguments)]
+    pub fn start_game_getcall(&self) -> starknet::core::types::Call {
+        use cainome::cairo_serde::CairoSerde;
+        let mut __calldata = vec![];
+        starknet::core::types::Call {
+            to: self.address,
+            selector: starknet::macros::selector!("start_game"),
+            calldata: __calldata,
+        }
+    }
+    #[allow(clippy::ptr_arg)]
+    #[allow(clippy::too_many_arguments)]
+    pub fn start_game(&self) -> starknet::accounts::ExecutionV3<A> {
+        use cainome::cairo_serde::CairoSerde;
+        let mut __calldata = vec![];
+        let __call = starknet::core::types::Call {
+            to: self.address,
+            selector: starknet::macros::selector!("start_game"),
+            calldata: __calldata,
+        };
+        self.account.execute_v3(vec![__call])
+    }
+    #[allow(clippy::ptr_arg)]
+    #[allow(clippy::too_many_arguments)]
+    pub fn submit_guess_getcall(
+        &self,
+        word_byte_array: &cainome::cairo_serde::ByteArray,
+    ) -> starknet::core::types::Call {
+        use cainome::cairo_serde::CairoSerde;
+        let mut __calldata = vec![];
+        __calldata.extend(cainome::cairo_serde::ByteArray::cairo_serialize(
+            word_byte_array,
+        ));
+        starknet::core::types::Call {
+            to: self.address,
+            selector: starknet::macros::selector!("submit_guess"),
+            calldata: __calldata,
+        }
+    }
+    #[allow(clippy::ptr_arg)]
+    #[allow(clippy::too_many_arguments)]
+    pub fn submit_guess(
+        &self,
+        word_byte_array: &cainome::cairo_serde::ByteArray,
+    ) -> starknet::accounts::ExecutionV3<A> {
+        use cainome::cairo_serde::CairoSerde;
+        let mut __calldata = vec![];
+        __calldata.extend(cainome::cairo_serde::ByteArray::cairo_serialize(
+            word_byte_array,
+        ));
+        let __call = starknet::core::types::Call {
+            to: self.address,
+            selector: starknet::macros::selector!("submit_guess"),
+            calldata: __calldata,
+        };
+        self.account.execute_v3(vec![__call])
     }
     #[allow(clippy::ptr_arg)]
     #[allow(clippy::too_many_arguments)]
@@ -253,36 +339,6 @@ impl<P: starknet::providers::Provider + Sync> CrosswordleCrosswordleReader<P> {
         let __call = starknet::core::types::FunctionCall {
             contract_address: self.address,
             entry_point_selector: starknet::macros::selector!("dojo_name"),
-            calldata: __calldata,
-        };
-        cainome::cairo_serde::call::FCall::new(__call, self.provider())
-    }
-    #[allow(clippy::ptr_arg)]
-    #[allow(clippy::too_many_arguments)]
-    pub fn start_game(&self) -> cainome::cairo_serde::call::FCall<P, ()> {
-        use cainome::cairo_serde::CairoSerde;
-        let mut __calldata = vec![];
-        let __call = starknet::core::types::FunctionCall {
-            contract_address: self.address,
-            entry_point_selector: starknet::macros::selector!("start_game"),
-            calldata: __calldata,
-        };
-        cainome::cairo_serde::call::FCall::new(__call, self.provider())
-    }
-    #[allow(clippy::ptr_arg)]
-    #[allow(clippy::too_many_arguments)]
-    pub fn submit_guess(
-        &self,
-        word_byte_array: &cainome::cairo_serde::ByteArray,
-    ) -> cainome::cairo_serde::call::FCall<P, ()> {
-        use cainome::cairo_serde::CairoSerde;
-        let mut __calldata = vec![];
-        __calldata.extend(cainome::cairo_serde::ByteArray::cairo_serialize(
-            word_byte_array,
-        ));
-        let __call = starknet::core::types::FunctionCall {
-            contract_address: self.address,
-            entry_point_selector: starknet::macros::selector!("submit_guess"),
             calldata: __calldata,
         };
         cainome::cairo_serde::call::FCall::new(__call, self.provider())
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use account_sdk::{
     provider,
     signers::{Owner, Signer},
 };
+use messing_with_controller::crosswordle_crosswordle::{
+    CrosswordleCrosswordle, CrosswordleCrosswordleReader,
+};
+// use messing_with_controller::crosswordle_crosswordle::CrosswordleCrosswordle;
 use starknet::{
     accounts::{Account, ExecutionEncoding, SingleOwnerAccount},
     core::types::{Call, Felt},
@@ -20,11 +24,12 @@ const CLASS_HASH: &str = "0x45575a88cc5cef1e444c77ce60b7b4c9e73a01cbbe20926d5a4c
 const CONTROLLER_ADDRESS: &str =
     "0x022997387672e7c476b338235b4b486b2d1e751d3833f035e514e26c5a80a6c6";
 const WORLD_ADDRESS: &str = "0x02f69de741bf133c6ffc205439bebdb5f26f0271dcc1cbb652b02878d33574da";
+const SELECTOR_ADDRESS: &str = "0x2ccc78c905d9fa42a542b8de8c438559ebac7eaecedf5cd932ffbd46fd98729";
 
 #[tokio::main]
 async fn main() {
-    let secret_scalar = Felt::from_hex_unchecked(PRIVATE_KEY);
-    let owner = Signer::Starknet(SigningKey::from_secret_scalar(secret_scalar));
+    // let secret_scalar = Felt::from_hex_unchecked(PRIVATE_KEY);
+    // let owner = Signer::Starknet(SigningKey::from_secret_scalar(secret_scalar));
 
     let provider = JsonRpcClient::new(HttpTransport::new(Url::parse(RPC_URL).unwrap()));
     let chain_id = provider.chain_id().await.unwrap();
@@ -36,21 +41,16 @@ async fn main() {
     let mut account =
         SingleOwnerAccount::new(provider, signer, address, chain_id, ExecutionEncoding::New);
 
-    let start_game_manual = account.execute_v1(vec![Call {
-        to: Felt::from_hex_unchecked(
-            "0x2ccc78c905d9fa42a542b8de8c438559ebac7eaecedf5cd932ffbd46fd98729",
-        ),
-        selector: starknet::core::utils::get_selector_from_name("start_game").unwrap(),
-        calldata: vec![],
-    }]);
+    let crosswordle =
+        CrosswordleCrosswordle::new(Felt::from_hex_unchecked(SELECTOR_ADDRESS), account);
+    let transaction1 = crosswordle.start_game().send().await.unwrap();
+    println!("{:?}", transaction1);
 
-    let make_guess = account.execute_v1(vec![Call {
-        to: Felt::from_hex_unchecked(
-            "0x2ccc78c905d9fa42a542b8de8c438559ebac7eaecedf5cd932ffbd46fd98729",
-        ),
-        selector: starknet::core::utils::get_selector_from_name("make_guess").unwrap(),
-        calldata: vec![],
-    }]);
-
-    let result = start_game_manual.send().await.unwrap();
+    // let provider_reader = JsonRpcClient::new(HttpTransport::new(Url::parse(RPC_URL).unwrap()));
+    // let cwreader = CrosswordleCrosswordleReader::new(
+    //     Felt::from_hex_unchecked(SELECTOR_ADDRESS),
+    //     provider_reader,
+    // );
+    // let transaction2 = cwreader.start_game().call().await.unwrap();
+    // println!("{:?}", transaction2);
 }
